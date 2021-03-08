2f4710cb0198ab70211bc01aa333e52118e14b80
sohel_rbs <sohel@rightbrainsolution.com>
2021-03-08 19:19:14 +0600

    Evaluation Criteria has been updated

--- Controller/EvaluationCriteriaController.php
+++ Controller/EvaluationCriteriaController.php
@@ -8,6 +8,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Terminalbd\CrmBundle\Entity\AntibioticFreeFarm;
+use Terminalbd\CrmBundle\Entity\ChickLifeCycle;
+use Terminalbd\CrmBundle\Entity\CostBenefitAnalysisForLessCostingFarm;
+use Terminalbd\CrmBundle\Entity\DiseaseMapping;
+use Terminalbd\CrmBundle\Entity\FarmerTrainingReport;
+use Terminalbd\CrmBundle\Entity\FcrDetails;
+use Terminalbd\CrmBundle\Entity\LayerLifeCycle;
+use Terminalbd\CrmBundle\Entity\LayerPerformanceDetails;
+use Terminalbd\CrmBundle\Entity\NewFarmerIntroduce\FarmerIntroduceDetails;
+use Terminalbd\CrmBundle\Entity\NewFarmerTouch\FarmerTouchReport;
 use Terminalbd\KpiBundle\Entity\EvaluationCriteria;
 use Terminalbd\KpiBundle\Form\EvaluationCriteriaFormType;
 
@@ -44,8 +54,7 @@ class EvaluationCriteriaController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted()){
-            /*            $data= $form->getData();
-                        dd($data);*/
+
             $em = $this->getDoctrine()->getManager();
 
             $ec->setCreatedAt(new \DateTime('now'));
@@ -101,5 +110,38 @@ class EvaluationCriteriaController extends AbstractController
         $this->addFlash('success', $translator->trans('Record has been deleted!'));
 
         return $this->redirectToRoute('evaluation_criteria_index');
+    }
+
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/evaluation-criteria-monthly-report", name="evaluation_criteria_monthly_report")
+     */
+    public function monthlyTotalReport()
+    {
+        $entities = [];
+        $filterBy = [];
+        $filterBy['employeeId'] = 21;
+        $filterBy['monthStart'] = '2021-02-01';
+        $filterBy['monthEnd'] = '2021-02-28';
+
+        $entities['totalFcrAfterSale'] = (int) $this->getDoctrine()->getRepository(FcrDetails::class)->getMonthlyFcrAfterSaleTotalReport($filterBy);
+        $totalBroilerBeforeSale = (int) $this->getDoctrine()->getRepository(FcrDetails::class)->getMonthlyBroilerBeforeSaleTotalReport($filterBy);
+        $totalLayerPerformance = (int) $this->getDoctrine()->getRepository(LayerPerformanceDetails::class)->getMonthlyLayerPerformanceTotalReport($filterBy);
+
+        $entities['broilerBeforeSaleAndLayerPerformance'] = $totalBroilerBeforeSale + $totalLayerPerformance;
+
+        $totalBroilerLifeCycle = (int) $this->getDoctrine()->getRepository(ChickLifeCycle::class)->getMonthlyBroilerLifeCycleTotalReport($filterBy);
+        $totalLayerLifeCycle = (int) $this->getDoctrine()->getRepository(LayerLifeCycle::class)->getMonthlyLayerLifeCycleTotalReport($filterBy);
+
+        $entities['broilerLifeCycleAndLayerLifeCycle'] = $totalBroilerLifeCycle + $totalLayerLifeCycle;
+
+        $entities['newFarmInformationOrSurvey'] = (int) $this->getDoctrine()->getRepository(FarmerTouchReport::class)->getMonthlyNewfarmInformationOrSurveyTotalReport($filterBy);
+        $entities['lessCostingFarmOrSkillFarmDevelop'] = (int) $this->getDoctrine()->getRepository(CostBenefitAnalysisForLessCostingFarm::class)->getMonthlyLessCostingFarmOrSkillFarmDevelopTotalReport($filterBy);
+        $entities['anitibioticFreeFarm'] = (int) $this->getDoctrine()->getRepository(AntibioticFreeFarm::class)->getMonthlyAntibioticFreeFarmTotalReport($filterBy);
+        $entities['farmersTrainingProgram'] = (int) $this->getDoctrine()->getRepository(FarmerTrainingReport::class)->getMonthlyfarmersTrainingProgramTotalReport($filterBy);
+        $entities['newFarmIntroduce'] = (int) $this->getDoctrine()->getRepository(FarmerIntroduceDetails::class)->getMonthlyNewFarmerIntroduceTotalReport($filterBy);
+        $entities['troubleshootingAndDiseases'] = (int) $this->getDoctrine()->getRepository(DiseaseMapping::class)->getMonthlyTroubleshootingAndDiseasesMappingTotalReport($filterBy);
+
+        dd($entities);
     }
 }
