82441c6cd3e5ae98dd4a2c89bc2fcfc060d74fff
sohel_rbs <sohel@rightbrainsolution.com>
2022-04-23 10:49:55 +0600

    check previous month KPI

--- Controller/EmployeeBoardController.php
+++ Controller/EmployeeBoardController.php
@@ -120,6 +120,15 @@ class EmployeeBoardController extends AbstractController
             $month = $monthYear[0];
             $year = $monthYear[1];
 
+            // check previous month KPI
+            $previousMonth = date('F', strtotime($data['employee_board_form']['monthYear'] . "last month"));
+            $findLastMonthKpi = $this->getDoctrine()->getRepository(EmployeeBoard::class)->findOneBy(['employee' => $emp, 'month' => $previousMonth, 'year' => $year]);
+            if (!$findLastMonthKpi){
+                $this->addFlash('warning', 'Please generate previous month KPI first!');
+                return $this->redirectToRoute('kpi_board_new');
+            }
+
+
             // Prevent next month
             if ($year > date('Y') || ($year == date('Y') && date('m', strtotime($month)) > date('m'))){
                 $this->addFlash('warning', 'Only allow previous or current month!');
